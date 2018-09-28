c9a8c865ddf154b29bfc12cb0243cc65e0c2dc1e
dongqi <dongqi@163.com>
2018-09-28 15:49:17 +0800

    添加项目

---
+++ src/main/java/com/example/one_demo/OneDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.one_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneDemoApplication.class, args);
+    }
+}
